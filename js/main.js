5c2fc53121b6857ff68b340b8bce2626c4684f25
Alaa Adel <54372974+alaaadel122@users.noreply.github.com>
2025-05-22 23:12:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,61 +1,61 @@
 var qoutesArray = [
      {
-          writterImg: '/img/oscar.jpg',
+          writterImg: './img/oscar.jpg',
           quote: '“Be yourself; everyone else is already taken.”',
           writter: '― Oscar Wilde'
      },
      {
-          writterImg: '/img/maeWest.jpg',
+          writterImg: './img/maeWest.jpg',
           quote: '“You only live once, but if you do it right, once is enough.”',
           writter: '― Mae West'
      },
      {
-          writterImg: '/img/drSeuss.jpg',
+          writterImg: './img/drSeuss.jpg',
           quote: '“You know you are in love when you cant fall asleep because reality is finally better than your dreams.”',
           writter: '― Dr. Seuss'
      },
      {
-          writterImg: '/img/frank.jpg',
+          writterImg: './img/frank.jpg',
           quote: '“So many books, so little time.”',
           writter: '― Frank Zappa'
      },
      {
-          writterImg: '/img/albert.jpg',
+          writterImg: './img/albert.jpg',
           quote: '“Two things are infinite: the universe and human stupidity; and I am not sure about the universe.”',
           writter: '― Albert Einstein'
      },
      {
-          writterImg: '/img/marcos.jpg',
+          writterImg: './img/marcos.jpg',
           quote: '“A room without books is like a body without a soul.”',
           writter: '― Marcus Tullius Cicero'
      },
      {
-          writterImg: '/img/william.jpg',
+          writterImg: './img/william.jpg',
           quote: "“You've gotta dance like there's nobody watching ,Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.”",
           writter: '― William W. Purkey'
      },
      {
-          writterImg: '/img/ralph.jpg',
+          writterImg: './img/ralph.jpg',
           quote: "“To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.”",
           writter: '― Ralph Waldo Emerson'
      },
      {
-          writterImg: '/img/gandhi.jpg',
+          writterImg: './img/gandhi.jpg',
           quote: "“Live as if you were to die tomorrow. Learn as if you were to live forever.”",
           writter: '― Mahatma Gandhi'
      },
      {
-          writterImg: '/img/albert.jpg',
+          writterImg: './img/albert.jpg',
           quote: "“A friend is someone who knows all about you and still loves you.”",
           writter: '― Elbert Hubbard'
      },
      {
-          writterImg: '/img/mark.jpg',
+          writterImg: './img/mark.jpg',
           quote: "“If you tell the truth, you don't have to remember anything.”",
           writter: '― Mark Twain'
      },
      {
-          writterImg: '/img/rebort.jpg',
+          writterImg: './img/rebort.jpg',
           quote: "“In three words I can sum up everything I've learned about life: it goes on.”",
           writter: '― Robert Frost'
      },
